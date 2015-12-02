50e892ac5a532e3582b4bb8238a1cb03dfeea7c5
Marc Delagrammatikas <delagrammatikas@gmail.com>
2015-12-01 16:52:24 -0800

    Add list of all letters and link ones with existing records.

--- Books/Models/BookListViewModel.cs
+++ Books/Models/BookListViewModel.cs
@@ -5,10 +5,12 @@ namespace Books.Models
 {
     public class BookListViewModel
     {
-        public IEnumerable<IGrouping<string, Book>> GroupedBooks;
+        public readonly IEnumerable<IGrouping<string, Book>> GroupedBooks;
+        public readonly List<string> Letters;
 
         public BookListViewModel(List<Book> books)
         {
+            this.Letters = new List<string> { "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" };
             this.GroupedBooks = books.GroupBy(book => book.Initial);
         }
     }
